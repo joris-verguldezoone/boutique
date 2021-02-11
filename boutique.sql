c34f870e1a0b9589e9c9f7763ff09d0131515a78
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-02-11 23:32:14 +0100

    sql

---
+++ boutique.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 11 fév. 2021 à 22:25
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `boutique`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresse`
+--
+
+DROP TABLE IF EXISTS `adresse`;
+CREATE TABLE IF NOT EXISTS `adresse` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int(11) NOT NULL,
+  `batiment` varchar(200) NOT NULL,
+  `rue` varchar(200) NOT NULL,
+  `code_postal` int(11) NOT NULL,
+  `pays` varchar(140) NOT NULL,
+  `ville` varchar(140) NOT NULL,
+  `info_sup` varchar(200) NOT NULL,
+  `tel` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(140) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `id_image` int(11) NOT NULL,
+  `id_image_type` int(11) NOT NULL,
+  `note` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `type` varchar(140) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `promo` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `carte_bleu`
+--
+
+DROP TABLE IF EXISTS `carte_bleu`;
+CREATE TABLE IF NOT EXISTS `carte_bleu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_carte` varchar(140) NOT NULL,
+  `nom` varchar(140) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `code` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gamme` varchar(140) NOT NULL,
+  `generation` varchar(140) NOT NULL,
+  `marque` varchar(140) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_adresse` int(11) NOT NULL,
+  `id_carte_bleu` int(11) NOT NULL,
+  `id_panier` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+DROP TABLE IF EXISTS `commentaires`;
+CREATE TABLE IF NOT EXISTS `commentaires` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(140) NOT NULL,
+  `contenu` varchar(140) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_like` int(11) NOT NULL,
+  `id_dislike` int(11) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likedislike`
+--
+
+DROP TABLE IF EXISTS `likedislike`;
+CREATE TABLE IF NOT EXISTS `likedislike` (
+  `id` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `id_commentaire` int(11) NOT NULL,
+  `likey` int(11) NOT NULL,
+  `dislike` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `liste_de_souhait`
+--
+
+DROP TABLE IF EXISTS `liste_de_souhait`;
+CREATE TABLE IF NOT EXISTS `liste_de_souhait` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marque`
+--
+
+DROP TABLE IF EXISTS `marque`;
+CREATE TABLE IF NOT EXISTS `marque` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(140) NOT NULL,
+  `id_image` int(11) NOT NULL,
+  `description` varchar(140) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notation`
+--
+
+DROP TABLE IF EXISTS `notation`;
+CREATE TABLE IF NOT EXISTS `notation` (
+  `id` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `id_commentaire` int(11) NOT NULL,
+  `note` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+
+DROP TABLE IF EXISTS `panier`;
+CREATE TABLE IF NOT EXISTS `panier` (
+  `id` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `id_image_article` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(140) NOT NULL,
+  `prenom` varchar(140) NOT NULL,
+  `pseudo` varchar(140) NOT NULL,
+  `email` varchar(140) NOT NULL,
+  `mdp` varchar(140) NOT NULL,
+  `id_droits` int(140) NOT NULL,
+  `anniversaire` date NOT NULL,
+  `id_adresse` int(11) NOT NULL,
+  `id_commentaire` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
