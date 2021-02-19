14e2ed3ab82f3126d828b8cac4f8eef004f4c882
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-02-19 10:39:42 +0100

    bdd

--- boutique.sql
+++ boutique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 17 fév. 2021 à 11:26
+-- Généré le :  ven. 19 fév. 2021 à 09:39
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -55,6 +55,8 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `presentation` varchar(255) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `image` varchar(255) NOT NULL,
+  `image_2` varchar(255) DEFAULT NULL,
+  `image_3` varchar(255) DEFAULT NULL,
   `note` int(11) DEFAULT NULL,
   `prix` int(11) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
@@ -65,14 +67,16 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `promo` int(11) DEFAULT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `articles`
 --
 
-INSERT INTO `articles` (`id`, `titre`, `presentation`, `description`, `image`, `note`, `prix`, `id_utilisateur`, `id_type`, `id_gamme`, `id_marque`, `id_generation`, `promo`, `date`) VALUES
-(1, '3090 FE', 'il etait une fois une carte', 'Elle a plus de coeur cuda que nimporte ki', 'https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png', NULL, 3090, 1, 3090, 3090, 3090, 3090, NULL, '2021-02-17');
+INSERT INTO `articles` (`id`, `titre`, `presentation`, `description`, `image`, `image_2`, `image_3`, `note`, `prix`, `id_utilisateur`, `id_type`, `id_gamme`, `id_marque`, `id_generation`, `promo`, `date`) VALUES
+(1, '3090 FE', 'il etait une fois une carte', 'Elle a plus de coeur cuda que nimporte ki', 'https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png', '', '', NULL, 3090, 1, 3090, 3090, 3090, 3090, NULL, '2021-02-17'),
+(4, '3060 FE', 'il etait une fois une carte', 'Elle a plus de coeur cuda que nimporte ki', 'https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png', 'https://user.oc-static.com/files/381001_382000/381064.png', 'http://localhost/phpmyadmin/themes/pmahomme/img/logo_left.png', NULL, 3060, 1, 3060, 3060, 3060, 3060, NULL, '2021-02-19'),
+(5, '3060 FE', 'il etait une fois une carte', 'Elle a plus de coeur cuda que nimporte ki', 'https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-300-t.png', 'https://user.oc-static.com/files/381001_382000/381064.png', 'http://localhost/phpmyadmin/themes/pmahomme/img/logo_left.png', NULL, 400, 1, 1, 1, 5, 1, NULL, '2021-02-19');
 
 -- --------------------------------------------------------
 
@@ -153,7 +157,7 @@ CREATE TABLE IF NOT EXISTS `gamme` (
   `id_type` int(11) NOT NULL,
   `id_marque` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='Cette table permet un tri facultatif selon l''article';
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci COMMENT='Cette table permet un tri facultatif selon l''article';
 
 --
 -- Déchargement des données de la table `gamme`
@@ -161,7 +165,16 @@ CREATE TABLE IF NOT EXISTS `gamme` (
 
 INSERT INTO `gamme` (`id`, `nom`, `id_type`, `id_marque`) VALUES
 (1, '3090', 1, 1),
-(2, 'I-9', 5, 2);
+(2, 'I-9', 5, 2),
+(3, '3070', 1, 1),
+(4, '3080', 1, 1),
+(5, '3060', 1, 1),
+(6, '2080', 1, 1),
+(7, '2070', 1, 1),
+(8, '2060', 1, 1),
+(9, '1660', 1, 1),
+(10, '1060', 1, 1),
+(11, '1050', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -176,7 +189,7 @@ CREATE TABLE IF NOT EXISTS `generation` (
   `id_type` int(11) NOT NULL,
   `id_marque` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `generation`
@@ -283,40 +296,22 @@ CREATE TABLE IF NOT EXISTS `type` (
   `nom` varchar(140) NOT NULL,
   `image` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `type`
 --
 
 INSERT INTO `type` (`id`, `nom`, `image`) VALUES
-(1, 'Carte Graphique', ''),
-(6, 'Intel', ''),
-(5, 'Processeur', ''),
-(7, 'AMD', '');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `type_article`
---
-
-DROP TABLE IF EXISTS `type_article`;
-CREATE TABLE IF NOT EXISTS `type_article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `type_article`
---
-
-INSERT INTO `type_article` (`id`, `type`) VALUES
-(1, 'RAM'),
-(2, 'RAM'),
-(3, 'RAM'),
-(4, 'RAM');
+(1, 'Carte Graphique', 'https://www.hardwarecooking.fr/wp-content/uploads/2020/11/msi-geforce-rtx-serie-3000-suprim.jpg'),
+(5, 'Processeur', 'https://images.idgesg.net/images/article/2018/03/blue-mother-board_circuitry_computer-chip_processor_harddrive-100751586-large.jpg'),
+(8, 'Stockage', 'https://hardzone.es/app/uploads-hardzone.es/2019/01/Magnetic-Hard-Drive-vs-SATA-SSD-vs-M2-NVMe.jpg'),
+(9, 'RAM', 'https://www.gamertech.fr/wp-content/uploads/2020/03/ram-memoire-pc-gamer.png'),
+(10, 'Ecran', 'https://images-na.ssl-images-amazon.com/images/I/71KS5M77puL._AC_SX425_.jpg'),
+(11, 'PC portable', 'https://image.jeuxvideo.com/medias-md/160647/1606471034-1133-card.jpg'),
+(12, 'PC fixe', 'https://www.hebergementwebs.com/image/d2/d20d03a174ae5af65c42ade1133329d1.jpg/obtenez-un-pc-de-jeu-pre-construit-rtx-3070-ou-rtx-3080-chez-newegg-13.jpg'),
+(13, 'Alimentation', 'https://www.alternate.fr/p/600x600/t/Corsair_CX_Series_CX750F_RGB_unit__d_alimentation_d__nergie_750_W_20_pin_ATX_ATX_Noir__Alimentation_PC@@tn7v6c04.jpg'),
+(14, 'Carte mère', 'https://www.rueducommerce.fr/media/images/web/produit/3211627/20210201072202/z590_vision_g_4_1140x1140.png');
 
 -- --------------------------------------------------------
 
