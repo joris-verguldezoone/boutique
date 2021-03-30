af02b5b09d5eb9f61b9315bb7fd1c1a8aa8f1963
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-03-30 10:55:56 +0200

    oublie

--- libraries/Controller/DisplayPartner.php
+++ libraries/Controller/DisplayPartner.php
@@ -5,17 +5,31 @@ require_once('Controller.php');
 
 class DisplayPartner extends Controller{
 
-    public function displayBrand($id_brand){
+
+    public function displayAllBrand($id_brand){
         $model = new \Model\DisplayPartner();
         $tab = $model->selectAllWhereFetchAll('marque', 'id', $id_brand);
         var_dump($tab);
     }
 
 
-    public function displayEditor($id_editor){
+    public function displayAllEditor($id_editor){
         $model = new \Model\DisplayPartner();
         
     }
+
+    public function displayOneBrand($id_brand){
+        $model = new \Model\DisplayPartner();
+        $tab = $model->selectAllWhereFetchAll('marque', 'id', $id_brand);
+        var_dump($tab);
+    }
+
+
+    public function displayOneEditor($id_editor){
+        $model = new \Model\DisplayPartner();
+        
+    }
+
 
 }
 
